1d10e7ff51d4aa1d940bf9553d01dd4c410def0f
Ivan Bobynets <ivan@osyah.com>
2024-05-27 13:46:32 +0300

    pletyvo: add QueryOption.String

--- pletyvo.go
+++ pletyvo.go
@@ -4,6 +4,9 @@
 package pletyvo
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/google/uuid"
 
 	"github.com/osyah/hryzun/status"
@@ -21,4 +24,50 @@ type QueryOption struct {
 	Order  bool
 	After  uuid.UUID
 	Before uuid.UUID
+}
+
+func (qo QueryOption) String() string {
+	var (
+		buf   strings.Builder
+		token byte = '?'
+	)
+
+	for i := 0; i < 4; i++ {
+		switch i {
+		case 0:
+			if qo.Limit != 0 {
+				buf.WriteByte(token)
+				buf.WriteString(fmt.Sprintf("limit=%d", qo.Limit))
+			} else {
+				continue
+			}
+		case 1:
+			if qo.Order {
+				buf.WriteByte(token)
+				buf.WriteString("order=asc")
+			} else {
+				continue
+			}
+		case 2:
+			if qo.After != uuid.Nil {
+				buf.WriteByte(token)
+				buf.WriteString("after=")
+				buf.WriteString(qo.After.String())
+			} else {
+				continue
+			}
+		case 3:
+			if qo.Before != uuid.Nil {
+				buf.WriteByte(token)
+				buf.WriteString("before=")
+				buf.WriteString(qo.Before.String())
+			} else {
+				continue
+			}
+		}
+
+		token = '&'
+	}
+
+	return buf.String()
 }
