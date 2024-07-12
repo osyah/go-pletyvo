353d53fc5825127c31be0818fdb84ee1a8b944b3
Ivan Bobynets <ivan@osyah.com>
2024-07-12 10:26:22 +0300

    protocol/delivery: add MessageHandler and NewMessageHandler

---
+++ protocol/delivery/message_handler.go
@@ -0,0 +1,48 @@
+// Copyright (c) 2024 Osyah
+// SPDX-License-Identifier: MIT
+
+package delivery
+
+import (
+	"context"
+
+	"github.com/osyah/hryzun/status"
+
+	"github.com/osyah/go-pletyvo"
+	"github.com/osyah/go-pletyvo/protocol/dapp"
+)
+
+type MessageHandler struct{ service *MessageExecutor }
+
+func NewMessageHandler(service *MessageExecutor) *MessageHandler {
+	return &MessageHandler{service: service}
+}
+
+func (mh MessageHandler) Handle(ctx context.Context, event *dapp.Event) error {
+	switch event.Body.Type().Event() {
+	case MessageCreate:
+		var input MessageCreateInput
+
+		if err := event.Body.Unmarshal(&input); err != nil {
+			return err
+		}
+
+		return mh.service.Create(ctx, dapp.EventBase[MessageCreateInput]{
+			EventHeader: event.EventHeader,
+			Input:       &input,
+		})
+	case MessageUpdate:
+		var input MessageUpdateInput
+
+		if err := event.Body.Unmarshal(&input); err != nil {
+			return err
+		}
+
+		return mh.service.Update(ctx, dapp.EventBase[MessageUpdateInput]{
+			EventHeader: event.EventHeader,
+			Input:       &input,
+		})
+	default:
+		return status.New(pletyvo.CodeInvalidArgument, "unsupported body type")
+	}
+}
