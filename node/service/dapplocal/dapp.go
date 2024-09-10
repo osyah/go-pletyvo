8bcc2686cc4be0a052eccb632f0150a5a81a1b4d
Ivan Bobynets <ivan@osyah.com>
2024-09-10 16:55:33 +0300

    node/service/dapplocal: add Config and Register

--- node/service/dapplocal/dapp.go
+++ node/service/dapplocal/dapp.go
@@ -4,10 +4,26 @@
 package dapplocal
 
 import (
+	"github.com/osyah/hryzun/container"
+
 	"github.com/osyah/go-pletyvo/node/relay"
 	"github.com/osyah/go-pletyvo/protocol/dapp"
 )
 
+type Config struct {
+	Repos string `cfg:"repos"`
+	Relay string `cfg:"relay"`
+}
+
 func New(repos *dapp.Repository, relay relay.Relay) *dapp.Service {
 	return &dapp.Service{Event: NewEvent(repos.Event, relay)}
+}
+
+func Register(config Config) container.Handler {
+	return func(base *container.Base) any {
+		return New(
+			container.Get[*dapp.Repository](base, config.Repos),
+			container.Get[relay.Relay](base, config.Relay),
+		)
+	}
 }
