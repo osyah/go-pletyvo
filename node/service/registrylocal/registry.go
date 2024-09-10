b05f71d37266c3c18a9e28743344fc3b1da408f7
Ivan Bobynets <ivan@osyah.com>
2024-09-10 16:58:15 +0300

    node/service/registrylocal: add Config and RegisterQuery

--- node/service/registrylocal/registry.go
+++ node/service/registrylocal/registry.go
@@ -3,8 +3,24 @@
 
 package registrylocal
 
-import "github.com/osyah/go-pletyvo/protocol/registry"
+import (
+	"github.com/osyah/hryzun/container"
+
+	"github.com/osyah/go-pletyvo/protocol/registry"
+)
+
+type Config struct {
+	Repos string `cfg:"repos"`
+}
 
 func NewQuery(repos *registry.Repository) *registry.Query {
 	return &registry.Query{Network: NewNetwork(repos.Network)}
+}
+
+func RegisterQuery(config Config) container.Handler {
+	return func(base *container.Base) any {
+		return NewQuery(
+			container.Get[*registry.Repository](base, config.Repos),
+		)
+	}
 }
