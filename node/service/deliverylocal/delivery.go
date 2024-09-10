d981b95c3b92314329440790b84ee3f0df77224d
Ivan Bobynets <ivan@osyah.com>
2024-09-10 16:59:37 +0300

    node/service/deliverylocal: add Config and RegisterQuery

--- node/service/deliverylocal/delivery.go
+++ node/service/deliverylocal/delivery.go
@@ -3,11 +3,27 @@
 
 package deliverylocal
 
-import "github.com/osyah/go-pletyvo/protocol/delivery"
+import (
+	"github.com/osyah/hryzun/container"
+
+	"github.com/osyah/go-pletyvo/protocol/delivery"
+)
+
+type Config struct {
+	Repos string `cfg:"repos"`
+}
 
 func NewQuery(repos *delivery.Repository) *delivery.Query {
 	return &delivery.Query{
 		Channel: NewChannel(repos.Channel),
 		Message: NewMessage(repos.Message),
+	}
+}
+
+func RegisterQuery(config Config) container.Handler {
+	return func(base *container.Base) any {
+		return NewQuery(
+			container.Get[*delivery.Repository](base, config.Repos),
+		)
 	}
 }
