8ecc2db01456f2bd6d2523a6c73011fe02de57aa
Ivan Bobynets <ivan@osyah.com>
2024-09-12 14:27:00 +0300

    repository/deliverypebble: add Register

--- repository/deliverypebble/pebble.go
+++ repository/deliverypebble/pebble.go
@@ -6,6 +6,7 @@ package deliverypebble
 import (
 	"github.com/VictoriaMetrics/easyproto"
 	"github.com/cockroachdb/pebble"
+	"github.com/osyah/hryzun/container"
 
 	"github.com/osyah/go-pletyvo/protocol/delivery"
 )
@@ -16,5 +17,13 @@ func New(db *pebble.DB) *delivery.Repository {
 	return &delivery.Repository{
 		Channel: NewChannel(db),
 		Message: NewMessage(db),
+	}
+}
+
+func Register() container.Handler {
+	return func(base *container.Base) any {
+		return New(
+			container.Get[*pebble.DB](base, "pebble"),
+		)
 	}
 }
