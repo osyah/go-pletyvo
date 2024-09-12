4accab871324a1434a79816901206de142d73f0f
Ivan Bobynets <ivan@osyah.com>
2024-09-12 14:26:03 +0300

    repository/registrypebble: add Register

--- repository/registrypebble/go.mod
+++ repository/registrypebble/go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/cockroachdb/pebble v1.1.0
 	github.com/google/uuid v1.6.0
 	github.com/osyah/go-pletyvo v0.0.0-00010101000000-000000000000
-	github.com/osyah/hryzun v0.0.0-20240502110742-a9c51ca6d5b4
+	github.com/osyah/hryzun v0.0.1
 )
 
 require (
--- repository/registrypebble/pebble.go
+++ repository/registrypebble/pebble.go
@@ -6,6 +6,7 @@ package registrypebble
 import (
 	"github.com/VictoriaMetrics/easyproto"
 	"github.com/cockroachdb/pebble"
+	"github.com/osyah/hryzun/container"
 
 	"github.com/osyah/go-pletyvo/protocol/registry"
 )
@@ -14,4 +15,12 @@ var mp easyproto.MarshalerPool
 
 func New(db *pebble.DB) *registry.Repository {
 	return &registry.Repository{Network: NewNetwork(db)}
+}
+
+func Register() container.Handler {
+	return func(base *container.Base) any {
+		return New(
+			container.Get[*pebble.DB](base, "pebble"),
+		)
+	}
 }
