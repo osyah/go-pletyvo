345a5fd3a4dfe0059fd13227e12179da7f29aa3d
Ivan Bobynets <ivan@osyah.com>
2024-10-07 16:37:22 +0300

    protocol: add Config and ConfigBase

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -8,6 +8,15 @@ import (
 	"github.com/osyah/go-pletyvo/protocol/registry"
 )
 
+type Config struct {
+	Registry ConfigBase `cfg:"registry"`
+	Delivery ConfigBase `cfg:"delivery"`
+}
+
+type ConfigBase struct {
+	Repos string `cfg:"repos"`
+}
+
 type Executor struct {
 	Registry *registry.Executor
 	Delivery *delivery.Executor
