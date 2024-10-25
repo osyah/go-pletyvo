41047a986dc3569a10d868af790026962cd968cd
Ivan Bobynets <ivan@osyah.com>
2024-10-25 21:36:13 +0300

    node/relay: add Register

--- node/relay/relay.go
+++ node/relay/relay.go
@@ -6,9 +6,20 @@ package relay
 import (
 	"context"
 
+	"github.com/osyah/hryzun/container"
+
+	"github.com/osyah/go-pletyvo/node/relay/localdoer"
 	"github.com/osyah/go-pletyvo/protocol/dapp"
 )
 
 type Relay interface {
 	OnEvent(context.Context, *dapp.Event) error
+}
+
+func Register(base *container.Base, config Config) {
+	if config.LocalDoer == nil {
+		panic("go-pletyvo/node/relay: 'local_doer' config not found")
+	} else {
+		base.RegisterHandler("local_doer", localdoer.Register(*config.LocalDoer))
+	}
 }
