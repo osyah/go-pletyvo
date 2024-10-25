c3a2908427a2dd5fc1e52843d9e38002b58f4dd3
Ivan Bobynets <ivan@osyah.com>
2024-10-25 21:33:58 +0300

    node/config: add Node.Store

--- node/config/node.go
+++ node/config/node.go
@@ -8,9 +8,11 @@ import (
 	"github.com/osyah/go-pletyvo/node/service"
 	"github.com/osyah/go-pletyvo/node/transport"
 	"github.com/osyah/go-pletyvo/protocol"
+	"github.com/osyah/go-pletyvo/store"
 )
 
 type Node struct {
+	Store     store.Config     `cfg:"store"`
 	Protocol  protocol.Config  `cfg:"protocol"`
 	Relay     relay.Config     `cfg:"relay"`
 	Service   service.Config   `cfg:"service"`
