424b67a235d199d14ca0f8c5f9e361a22746488a
Ivan Bobynets <ivan@osyah.com>
2024-12-20 00:05:22 +0200

    node/config: add ProtocolConfig and ProtocolBase

--- node/config/node.go
+++ node/config/node.go
@@ -7,14 +7,21 @@ import (
 	"github.com/osyah/go-pletyvo/node/relay"
 	"github.com/osyah/go-pletyvo/node/service"
 	"github.com/osyah/go-pletyvo/node/transport"
-	"github.com/osyah/go-pletyvo/protocol"
 	"github.com/osyah/go-pletyvo/store"
 )
 
 type Node struct {
 	Store     store.Config     `cfg:"store"`
-	Protocol  protocol.Config  `cfg:"protocol"`
+	Protocol  ProtocolConfig   `cfg:"protocol"`
 	Relay     relay.Config     `cfg:"relay"`
 	Service   service.Config   `cfg:"service"`
 	Transport transport.Config `cfg:"transport"`
+}
+
+type ProtocolConfig struct {
+	Delivery ProtocolBase `cfg:"delivery"`
+}
+
+type ProtocolBase struct {
+	Repos string `cfg:"repos"`
 }
