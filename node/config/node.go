feb46d1a060c95b9c6ef83062536d61a61f7b3f6
Ivan Bobynets <ivan@osyah.com>
2024-12-30 23:09:24 +0200

    node/config: update Node and rename ProtocolConfig

--- node/config/node.go
+++ node/config/node.go
@@ -6,19 +6,19 @@ package config
 import (
 	"github.com/osyah/go-pletyvo/node/relay"
 	"github.com/osyah/go-pletyvo/node/service"
+	"github.com/osyah/go-pletyvo/node/store"
 	"github.com/osyah/go-pletyvo/node/transport"
-	"github.com/osyah/go-pletyvo/store"
 )
 
 type Node struct {
 	Store     store.Config     `cfg:"store"`
-	Protocol  ProtocolConfig   `cfg:"protocol"`
+	Protocol  Protocol         `cfg:"protocol"`
 	Relay     relay.Config     `cfg:"relay"`
 	Service   service.Config   `cfg:"service"`
 	Transport transport.Config `cfg:"transport"`
 }
 
-type ProtocolConfig struct {
+type Protocol struct {
 	Delivery ProtocolBase `cfg:"delivery"`
 }
 
