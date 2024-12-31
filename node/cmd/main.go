2fffb4daeb297058bc3726beedcb93fccca069b3
Ivan Bobynets <ivan@osyah.com>
2024-12-31 16:23:05 +0200

    node/cmd: add default network support

--- node/cmd/main.go
+++ node/cmd/main.go
@@ -5,9 +5,11 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/rs/zerolog/log"
 
+	"github.com/osyah/go-pletyvo"
 	"github.com/osyah/go-pletyvo/node"
 )
 
@@ -19,6 +21,18 @@ func init() {
 			"This flag is required to run the application.",
 	)
 	flag.Parse()
+
+	input := os.Getenv("PLETYVO_DEFAULT_NETWORK")
+	if len(input) != 0 {
+		network, err := pletyvo.NetworkFromString(input)
+		if err != nil {
+			log.Fatal().Err(err).Msg(
+				"Invalid value format in 'PLETYVO_DEFAULT_NETWORK' environment.",
+			)
+		}
+
+		copy(pletyvo.DefaultNetwork[:], network[:])
+	}
 }
 
 func main() {
