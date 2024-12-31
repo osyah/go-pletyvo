519b93571773c0feb57c227442c51caa667bf7ca
Ivan Bobynets <ivan@osyah.com>
2024-12-31 16:19:02 +0200

    pletyvo: add Network.String, NetworkFromString and DecodeNetwork

--- network.go
+++ network.go
@@ -3,8 +3,50 @@
 
 package pletyvo
 
-const NetworkSize = 6
+import (
+	"encoding/base64"
 
-var DefaultNetwork, NetworkNil Network
+	"github.com/osyah/hryzun/status"
+)
+
+const (
+	NetworkSize = 6
+	NetworkLen  = 8
+)
+
+var (
+	DefaultNetwork, NetworkNil Network
+
+	ErrInvalidNetworkFormat = status.New(
+		CodeInvalidArgument, "invalid network format",
+	)
+)
 
 type Network [NetworkSize]byte
+
+func (n Network) String() string {
+	return base64.RawURLEncoding.EncodeToString(n[:])
+}
+
+func NetworkFromString(s string) (Network, error) {
+	var network Network
+
+	if len(s) != NetworkLen {
+		return NetworkNil, ErrInvalidNetworkFormat
+	}
+
+	return network, DecodeNetwork(network[:], []byte(s))
+}
+
+func DecodeNetwork(dst []byte, src []byte) error {
+	n, err := base64.RawURLEncoding.Decode(dst, src)
+	if err != nil {
+		return err
+	}
+
+	if n != NetworkSize {
+		return ErrInvalidNetworkFormat
+	}
+
+	return nil
+}
